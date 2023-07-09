83e18e910b8dbaad6a6adfc35676ef1071c9f0a5
Anastasiia-Berehovykh <anastanber@gmail.com>
2023-07-09 22:22:42 +0300

    update joi

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -2,8 +2,8 @@ const Joi = require('joi');
 
 const addSchema = Joi.object({
   name: Joi.string().min(3).required(),
-  email: Joi.string().required(),
-  phone: Joi.string().min(6).required(),
+  email: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/).required(),
+  phone: Joi.string().regex(/^[0-9]{10}$/).messages({'string.pattern.base': `Phone number must have 10 digits.`}).required(),
 });
 
 module.exports = { addSchema };
