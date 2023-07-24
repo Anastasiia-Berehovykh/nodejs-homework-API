1910844b83ed68a52c9e456b4ebd0cd6ea93c515
Anastasiia-Berehovykh <anastanber@gmail.com>
2023-07-24 23:13:05 +0300

    finish

---
+++ middlewares/index.js
@@ -0,0 +1,9 @@
+const isValidId = require("./isValidid");
+const validateBody = require("./validateBody");
+const auth = require("./auth");
+
+module.exports = {
+  isValidId,
+  validateBody,
+  auth,
+};
---
+++ routes/api/contact.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const ctrl = require("../../controllers/contacts");
+const {auth, validateBody, isValidId} = require("../../middlewares");
+const { schemas } = require("../../models/contact");
+
+
+const router = express.Router();
+
+router.get("/", auth, ctrl.getAll);
+
+router.get("/:id", auth, isValidId, ctrl.getById);
+
+router.post("/", auth, validateBody(schemas.addSchema), ctrl.add);
+
+router.put("/:id", auth, isValidId, validateBody(schemas.addSchema), ctrl.updateById);
+
+router.patch("/:id/favorite", auth, isValidId, validateBody(schemas.updateFavoriteSchema), ctrl.updateFavorite);
+
+router.delete("/:id", auth, isValidId, ctrl.deleteById);
+
+module.exports = router;
